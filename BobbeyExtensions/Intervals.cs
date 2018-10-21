8f40426c773ab67b10dd54cc633aa7704f45bc31
Maico Ayissi <md.ayissi@web.de>
2018-10-21 13:05:33 +0200

    Added method which parses an Interval as defined in ISO 31-11

---
+++ BobbeyExtensions/Intervals.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BobbeyExtensions
+{
+    public static class Intervals
+    {
+        /// <summary>
+        /// Determins if an Integer is within the given range which includes its defining borders.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="limit1"></param>
+        /// <param name="limit2"></param>
+        /// <returns></returns>
+        public static bool IsInClosedInterval(this int number, int limit1, int limit2)
+        {            
+            int upperlimit = limit2;
+            int lowerlimit = limit1;
+
+            if (limit1 > limit2)
+            {
+                upperlimit = limit1;
+                lowerlimit = limit2;
+            }
+            
+
+            if (number > upperlimit || number < lowerlimit)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Determins if an Integer is within the given range which excludes its defining borders.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="limit1"></param>
+        /// <param name="limit2"></param>
+        /// <returns></returns>
+        public static bool IsInOpenInterval(this int number, int limit1, int limit2)
+        {
+
+            int upperlimit = limit2;
+            int lowerlimit = limit1;
+
+            if (limit1 > limit2)
+            {
+                upperlimit = limit1;
+                lowerlimit = limit2;
+            }
+
+
+            if (number >= upperlimit || number <= lowerlimit)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        /// <summary>
+        /// Determins if the given Integer is contained by the defined interval.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="interval">Well defined interval as specified in ISO 31-11</param>
+        /// <returns>bool isInInterval</returns>
+        public static bool IsInInterval(this int number, string interval)
+        {
+
+            var x = Regex.Match(interval, @"(\[|\()(\d+)\,(\d+)(\)|\])");
+
+            if (!x.Success)
+            {
+                throw new ArgumentException("given string is not a well formed interval as specified in ISO 31-11.", nameof(interval));
+            }
+
+            bool isTopOpen = x.Groups[4].Value != "]";
+            bool IsBottomOpen = x.Groups[1].Value != "[";
+
+
+            int lowerLimit = int.Parse(x.Groups[2].Value);
+            int upperLimit = int.Parse(x.Groups[3].Value);
+
+            if (upperLimit < lowerLimit)
+            {
+                throw new ArgumentException("given string is not well formed as specified in ISO 31-11. Check the order of your Parameters, try to switch them.", nameof(interval));
+            }
+
+
+            if (IsBottomOpen)
+            {
+                if (number <= lowerLimit)
+                {
+                    return false;
+                }
+            } else {
+
+                if (number < lowerLimit)
+                {
+                    return false;
+                }
+            }
+
+            if (isTopOpen)
+            {
+                if (number >= upperLimit)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (number > upperLimit)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ ExtensionsUnitTests/IsInInterval.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Xunit;
+using BobbeyExtensions;
+
+namespace ExtensionsUnitTests
+{
+    public class IsInInterval
+    {
+        [Fact]
+        public void IsFiveInTwoToFive()
+        {
+
+            bool result = 5.IsInInterval("[2,5]");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsFiveInTwoToFiveBottomOpen()
+        {
+
+            bool result = 5.IsInInterval("(2,5]");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsZeroInTwoToFiveBottomOpen()
+        {
+
+            bool result = 0.IsInInterval("(2,5]");
+
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void IsHundredTwentyThreeInTwentyThreeToHundredTwentyThreeBottomOpen()
+        {
+
+            bool result = 123.IsInInterval("(23,123]");
+
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void IsHundredTwentyThreeInTwentyThreeToHundredTwentyThreeOpen()
+        {
+
+            bool result = 123.IsInInterval("(23,123)");
+
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void IsMinusOneInTwentyThreeToHundredTwentyThreeOpen()
+        {
+
+            bool result = (-1).IsInInterval("(23,123)");
+
+            Assert.False(result);
+        }
+
+
+        [Fact]
+        public void ThrowsException()
+        {
+
+            try
+            {
+                bool result = 10.IsInInterval("(123,32)");
+            }
+            catch (Exception e)
+            {
+                Assert.IsType<ArgumentException>(e);
+            }
+           
+        }
+    }
+}
